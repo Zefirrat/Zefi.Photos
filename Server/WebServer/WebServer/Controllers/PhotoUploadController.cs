0982ab45041623845ad331f69fc65ac5cbeefaf0
Zefirrat <leonidpavlov314@gmail.com>
2021-06-05 16:27:22 +0500

    Initial commit of server

---
+++ Server/WebServer/WebServer/Controllers/PhotoUploadController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PhotoUploadController : ControllerBase
+    {
+        private readonly ILogger<PhotoUploadController> _logger;
+
+        public PhotoUploadController(ILogger<PhotoUploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("TestConnection")]
+        public IActionResult TestConnection()
+        {
+            _logger.LogInformation("TestConnection working");
+            return Ok();
+        }
+    }
+}
